1284189c644cd936f5aa0de26c689fa0748fd236
Yofré López <53783795+yofrelopez@users.noreply.github.com>
2026-07-25 15:20:03 -0500

    Change icon source and clean up manifest
    Updated icon source in manifest and removed unused icons.

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,16 +14,9 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icon.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/icons/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
       },
     ],
   };
